1f32e75017abf98d79425c50ad8cef36a1a654b2
EstefanoCampana <Estefano.CampanaMunoz@edu.sait.ca>
2025-04-03 18:07:40 -0600

    Add files via upload

---
+++ Assignment3 LinkedList/Utility/Exception.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_LinkedList
+{
+    public class ListIndexOutOfRangeException : Exception
+    {
+        public ListIndexOutOfRangeException() : base("Target Index Out of Bounds For Linked List.") { }
+    }
+
+    public class EmptyListException : Exception
+    {
+        public EmptyListException() : base("The list is empty.") { }
+        public EmptyListException(int index) : base("The inputted value is out of range for the linked list.") { }
+    }
+}
